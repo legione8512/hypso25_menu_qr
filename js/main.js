6df51a7afdc53da5755a69721d669fdccf6085dd
legione8512 <marius.robert@gmail.com>
2025-07-16 15:36:17 +0300

    started cleaning the css code

--- js/main.js
+++ js/main.js
@@ -13,29 +13,5 @@
         $('html, body').animate({scrollTop: 0}, 1500, 'swing');
         return false;
     });
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        margin: 30,
-        dots: true,
-        loop: true,
-        center: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
     
 })(jQuery);
